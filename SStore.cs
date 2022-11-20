ec8aabb0fd343bd1ef6708961df483dcb2eea5e2
tejaswi1011 <68186241+tejaswi1011@users.noreply.github.com>
2022-11-20 14:53:27 -0600

    Add files via upload

---
+++ SStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal sealed class SStore
+    {
+        private static int privatePageNum = 0;
+        public static int page
+
+        { 
+            get
+
+            {
+                return privatePageNum;
+
+            }
+
+        }
+
+
+
+        public static void set(int p)
+
+        {
+
+            privatePageNum = p;
+
+        }
+    }
+}
